44975be6aaf01fa10d3679eae544454fb80608a5
Rita Gr <ritagrushovets2002@gmail.com>
2024-12-01 08:03:02 +0200

    fix mobile panel

--- panel.js
+++ panel.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const panelTrigger = document.querySelector(".panel-container");
+    const mainPanel = document.querySelector(".main-content__panel-container");
     const panelIcon = document.querySelector(".panel-container__panel-icon");
     const mainContent = document.querySelector(".main-content");
     const footerContent = document.querySelector(".footer");
     const map = document.querySelector(".map-mobile");
     const notificationContainer = document.querySelector(".notification-container__item");
-  
+    const mobileFooter = document.querySelector(".main-content__footer-mobile");
     if (!panelTrigger || !panelIcon) {
       console.error("Panel trigger or icon element not found.");
       return;
@@ -17,10 +18,24 @@ document.addEventListener("DOMContentLoaded", () => {
       if (isMobile()) {
         mainContent.classList.toggle("active");
         panelTrigger.classList.toggle("at-top");
+        mainPanel.classList.toggle("visible");
         footerContent.classList.toggle("active");
         map.classList.toggle("hidden");
-        document.body.classList.toggle("active");
+        // document.body.classList.toggle("active");
         notificationContainer.classList.toggle("hidden");
+        mobileFooter.classList.toggle("visible");
+      }
+    });
+    mainPanel.addEventListener("click", () => {
+      if (isMobile()) {
+        mainContent.classList.toggle("active");
+        panelTrigger.classList.toggle("at-top");
+        mainPanel.classList.toggle("visible");
+        footerContent.classList.toggle("active");
+        map.classList.toggle("hidden");
+        // document.body.classList.toggle("active");
+        notificationContainer.classList.toggle("hidden");
+        mobileFooter.classList.toggle("visible");
       }
     });
   });
